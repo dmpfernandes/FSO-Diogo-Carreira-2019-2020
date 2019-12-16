62ceeadc810f57266c069bd4a6e4ebd28d4be50f
Miguel Carreira <miguel.carreira7@gmail.com>
2019-12-16 20:36:44 +0000

    ACCOES

---
+++ FSO-tp2/src/ACCOES.java
@@ -0,0 +1,23 @@
+
+public enum ACCOES {
+	PARAR_FALSE(0), RETA(1), CDIR(2), CESQ(3), BACK(4), PARAR_TRUE(5), CLOSE(6);
+	private int value;
+
+	private ACCOES(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public static ACCOES getAcaoWithValue(int value) {
+		for(ACCOES acao : ACCOES.values()) {
+			if(acao.value == value) {
+				return acao;
+			}
+		}
+		return null;
+	}
+	
+}
